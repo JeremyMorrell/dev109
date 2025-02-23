aa2ba04a684b1b3880f425d4f415712ef8c148cf
JeremyMorrell <jjowls@icloud.com>
2025-02-22 18:53:13 -0800

    Update rhombus.js

--- a3/rhombus.js
+++ a3/rhombus.js
@@ -5,76 +5,74 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
     downRight(pHeight, pColorEven, pColorOdd, pSymbol);
 }
 
-function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
     var rLine = "";
-    for (var i = 0; i < pHeight; i++) {
+    for (i=0;i<pHeight;i++){
         rLine += "<p>";
+        
+        for(j=pHeight;j>i;j--){
+            rLine += "&nbsp;&nbsp;"; 
+        }
       
-        for (var j = pHeight - i; j > 0; j--) {
-           
-            if (j % 2) {
+        for(j=0;j<=i;j++){
+            if (j%2)
                 rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-            } else {
+            else
                 rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
-            }
         }
         rLine += "</p>";
     }
     document.getElementById("upLeft").innerHTML = rLine;
 }
 
-function downLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
     var rLine = "";
-    for (var i = pHeight; i > 0; i--) {
+    for (i=pHeight;i > 0;i--){
         rLine += "<p>";
-       
-        for (var j = pHeight - i + 1; j > 0; j--) {
-           
-            if (j % 2) {
+        
+        for(j=pHeight;j>=i;j--){
+            rLine += "&nbsp;&nbsp;"; 
+        }
+
+        for(j=0;j<i;j++){
+            if (j%2)
                 rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-            } else {
+            else
                 rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
-            }
         }
         rLine += "</p>";
     }
     document.getElementById("downLeft").innerHTML = rLine;
 }
 
-function upRight(pHeight, pColorEven, pColorOdd, pSymbol) {
+function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
     var rLine = "";
-    for (var i = 0; i < pHeight; i++) {
+    for (i=0;i<pHeight;i++){
         rLine += "<p>";
-      
-        for (var j = 0; j <= i; j++) {
-            
-            if (j % 2) {
+       
+        for(j=0;j<=i;j++){
+            if (j%2)
                 rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-            } else {
+            else
                 rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
-            }
         }
         rLine += "</p>";
     }
     document.getElementById("upRight").innerHTML = rLine;
 }
 
-function downRight(pHeight, pColorEven, pColorOdd, pSymbol) {
+function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
     var rLine = "";
-    for (var i = pHeight; i > 0; i--) {
+    for (i=pHeight;i > 0;i--){
         rLine += "<p>";
-        
-        for (var j = 0; j < i; j++) {
-            
-            if (j % 2) {
+
+        for(j=0;j<i;j++){
+            if (j%2)
                 rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-            } else {
+            else
                 rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
-            }
         }
         rLine += "</p>";
     }
     document.getElementById("downRight").innerHTML = rLine;
 }
-
-
