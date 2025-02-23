ad7b3f8c29b5221c0f44bf73f2ae04a816a0723a
JeremyMorrell <jjowls@icloud.com>
2025-02-22 23:32:02 -0800

    Update rhombus.js

--- a3/rhombus.js
+++ a3/rhombus.js
@@ -1,73 +1,76 @@
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
-    upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
-    upRight(pHeight, pColorEven, pColorOdd, pSymbol);
-    downLeft(pHeight, pColorEven, pColorOdd, pSymbol);
-    downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+upRight(pHeight, pColorEven, pColorOdd, pSymbol);
+downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+
 }
 
-function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
-    let rLine = "";
-    for (let i = 0; i < pHeight; i++) {
-        rLine += "<p>";
-        for (let j = pHeight - 1; j > i; j--) {
-            rLine += "&nbsp;"; 
-        }
-        for (let j = 0; j <= i; j++) {
-            if (j % 2 == 0) 
-                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
-            else 
-                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-        }
-        rLine += "</p>";
-    }
-    document.getElementById("upLeft").innerHTML = rLine;
+function upRight(pHeight, pColorEven, pColorOdd, pSymbol){ 
+
+
+.rhombus-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
 }
 
-function downLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
-    let rLine = "";
-    for (let i = pHeight - 1; i >= 0; i--) {
-        rLine += "<p>";
-        for (let j = pHeight - 1; j > i; j--) {
-            rLine += "&nbsp;"; 
-        }
-        for (let j = 0; j <= i; j++) {
-            if (j % 2 == 0) 
-                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
-            else 
-                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-        }
-        rLine += "</p>";
-    }
-    document.getElementById("downLeft").innerHTML = rLine;
+#upRight, #downRight {
+  font-family: monospace;
+  text-align: left;
+  margin: 0 10px;
+  white-space: pre; 
 }
 
-function upRight(pHeight, pColorEven, pColorOdd, pSymbol) {
-    let rLine = "";
-    for (let i = 0; i < pHeight; i++) {
-        rLine += "<p>";
-        for (let j = 0; j <= i; j++) {
-            if (j % 2 == 0) 
-                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
-            else 
-                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-        }
-        rLine += "</p>";
-    }
-    document.getElementById("upRight").innerHTML = rLine;
+body {
+  text-align: center;
+  font-family: Arial, sans-serif;
 }
 
-function downRight(pHeight, pColorEven, pColorOdd, pSymbol) {
-    let rLine = "";
-    for (let i = pHeight - 1; i >= 0; i--) {
-        rLine += "<p>";
-        for (let j = 0; j <= i; j++) {
-            if (j % 2 == 0) 
-                rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
-            else 
-                rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-        }
-        rLine += "</p>";
-    }
-    document.getElementById("downRight").innerHTML = rLine;
+h1 {
+  margin-bottom: 20px;
+}
+ 
+var rLine ="";
+for (i=0;i<pHeight;i++){
+rLine +="<p>";
+
+for(j=0;j<=i;j++){
+
+
+if (j%2)
+
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+
+
 }
 
+document.getElementById("upRight").innerHTML = rLine;
+}
+
+function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=pHeight;i > 0;i--){
+rLine +="<p>";
+
+for(j=0;j<i;j++){
+
+if (j%2)
+
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+
+
+}
+
+document.getElementById("downRight").innerHTML = rLine;
+}
