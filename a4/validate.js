e5e06f559ce49c7ef9d6460a0219ac658ff040f0
JeremyMorrell <jjowls@icloud.com>
2025-02-25 20:53:12 -0800

    Update validate.js

--- a4/validate.js
+++ a4/validate.js
@@ -1,1 +1,18 @@
+function isValid() {
+   
+    document.getElementById('fname').innerHTML = "";
+
+    let formIsValid = true;
+
+   
+    const firstName = document.getElementById('FirstName').value;
+
+   
+    if (firstName === "" || firstName.length > 20 || !/^[a-zA-Z]+$/.test(firstName)) {
+        document.getElementById('fname').innerHTML = "First name is required, must be alphabetical, and cannot exceed 20 characters.";
+        formIsValid = false;
+    }
+
+    return formIsValid;
+}
 
